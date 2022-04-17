8e4996602daeb13eafd59bdb6b9ff2c7724996d0
PrantaDas <prantodas043@gmail.com>
2022-04-18 05:52:10 +0600

    error handling done

--- src/Components/Pages/Login/Login.js
+++ src/Components/Pages/Login/Login.js
@@ -4,57 +4,93 @@ import { Link, useNavigate } from 'react-router-dom';
 import SocialLogin from '../SocialLogin/SocialLogin';
 import './Login.css'
 import auth from '../../../firebase.init';
-import { useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
-    const [sendPasswordResetEmail, sending, error] = useSendPasswordResetEmail(
-        auth
-      );
-      const [email,setEmail]=useState({value:"",error:"",error1:""});
-      const [password, setPassword] = useState({ value: "", error: "" });
-      const navigate=useNavigate();
-      const handleEmail = (event) => {
+    const [email, setEmail] = useState({ value: "", error1: "" });
+    const [password, setPassword] = useState({ value: "", error2: "" });
+    const navigate = useNavigate();
+    const handleEmail = (event) => {
         const emailInput = event.target.value;
-        if (/\S+@\S+\.\S+/.test(emailInput)) {
-            setEmail({ value: emailInput, error: "",error1:"" })
-        }
-        else {
-            setEmail({ email: "", error: "Please Provide Your Email",error1:"" });
-        }
+        setEmail({ value: emailInput, error1: "" });
     }
     const handlePassword = (event) => {
         const passInput = event.target.value;
-        let pattern = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
-        if (!pattern.test(passInput)) {
-            setPassword({ value: "", error: "Please Input Your Valid Password" })
-        }
-        else {
-            setPassword({ value: passInput, error: "" });
-        }
+
+        setPassword({ value: passInput, error2: "" });
+
     }
 
-    const handleResetPassword=()=>{
-        if(email.value===""){
-            setEmail({value:"",error:"",error1:"Please Provide Your Email First"});
+    const handleResetPassword = () => {
+        if (email.value === "") {
+            setEmail({ value: "", error1: "Please Provide Your Email First" });
         }
-        else{
-            sendPasswordResetEmail(email.value);
-            toast.success('Reset Request Sent!',{
-                theme:'colored',delay:1000
+        else {
+            sendPasswordResetEmail(auth, email.value)
+                .then(() => {
+                    toast.success("Reset Request Sent !", {
+                        theme: 'colored'
+                    })
+                })
+                .catch(error => {
+                    console.log(error, 'error');
+                    const errorMessage = error.message;
+                    if (errorMessage.includes('auth/user-not-found')) {
+                        toast.error('User Not Found', {
+                            theme: 'colored'
+                        })
+                    }
+
+                })
+        }
+
+    }
+
+
+    const handleLogin = (event) => {
+        event.preventDefault();
+        if (email.value && password.value === "") {
+            toast.warning('Input eamil and password first', {
+                theme: 'colored'
             })
         }
-            
+
+        if (email.value && password.value) {
+            signInWithEmailAndPassword(auth, email.value, password.value)
+                .then(userCredential => {
+                    navigate('/');
+                })
+                .catch((error) => {
+                    const errorMessage = error.message;
+                    console.log(error);
+                    if (errorMessage.includes('auth/wrong-password')) {
+                        toast.error('Invalid Password', {
+                            theme: 'colored'
+                        })
+                    }
+                    if (errorMessage.includes('auth/user-not-found')) {
+                        toast.error('User Not Found', {
+                            theme: 'colored'
+                        })
+                    }
+                })
+
         }
-    
+
+    }
+
     return (
         <div className='container w-50 mx-auto mt-5 shadow-lg p-5 rounded-3 border'>
             <h2 className='text-primary fw-bold form-title'>Please Login!!</h2>
-            <Form >
+            <Form onSubmit={handleLogin}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control onChange={handleEmail} className='' type="email" placeholder="Enter email" required />
+                    {/* {
+                            email.error && (<p className='text-danger' mt-2>{email.error}</p>)
+                        } */}
                     {
                         email.error1 && (<p className='text-danger' mt-2>{email.error1}</p>)
                     }
@@ -65,10 +101,13 @@ const Login = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control onClick={handlePassword} className='' type="password" placeholder="Password" required />
+                    <Form.Control onChange={handlePassword} className='' type="password" placeholder="Password" required />
+                    {
+                        password.error2 ? (<p className='text-danger' mt-2>{password.error2}</p>) : ""
+                    }
                 </Form.Group>
                 <Button variant="primary" type="submit">
-                    Submit
+                    LogIn
                 </Button>
             </Form>
             <p className='alter-options'><small>New to Healthy Bite?</small><Link className='text-decoration-none ps-1' to='/signup'>Register Here.</Link></p>
